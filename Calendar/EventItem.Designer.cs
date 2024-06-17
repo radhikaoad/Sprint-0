c360dd413549ff5da2ece95dc21e0e92f54e13fb
trippie <109394821+rayyankhann@users.noreply.github.com>
2024-06-16 19:18:07 -0500

    Update EventItem.Designer.cs

--- Calendar/EventItem.Designer.cs
+++ Calendar/EventItem.Designer.cs
@@ -2,14 +2,14 @@
 {
     partial class EventItem
     {
-        /// <summary> 
+     
         /// Required designer variable.
-        /// </summary>
+        
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary> 
+       
         /// Clean up any resources being used.
-        /// </summary>
+        
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
@@ -22,10 +22,10 @@
 
         #region Component Designer generated code
 
-        /// <summary> 
+     
         /// Required method for Designer support - do not modify 
         /// the contents of this method with the code editor.
-        /// </summary>
+       
         private void InitializeComponent()
         {
                this.location = new System.Windows.Forms.Label();
