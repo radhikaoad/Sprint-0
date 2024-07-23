2f0fa64c8cedd0adf6d8dc9d82154eb176364588
trippie <109394821+rayyankhann@users.noreply.github.com>
2024-07-23 00:21:04 -0500

    Update RoundedControlButton.Designer.cs

--- RoundedControlButton.Designer.cs
+++ RoundedControlButton.Designer.cs
@@ -21,7 +21,7 @@
         }
 
         #region Component Designer generated code
-
+ 
         /// <summary> 
         /// Required method for Designer support - do not modify 
         /// the contents of this method with the code editor.
