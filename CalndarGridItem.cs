181ffdf2409cf1cddf630f2d9b6aa15834270697
trippie <109394821+rayyankhann@users.noreply.github.com>
2024-07-23 00:15:11 -0500

    Update CalndarGridItem.cs

--- CalndarGridItem.cs
+++ CalndarGridItem.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 
 namespace Calendar
 {
-     public partial class CalndarGridItem : UserControl
+     public partial class CalndarGridItem : UserControl 
      {
           private DateTime date;
 
