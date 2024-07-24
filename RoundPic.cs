76869cc6ca0d5e9c8aad44b1146754ae8b240b87
edwinspn <151068422+edwinspn@users.noreply.github.com>
2024-07-24 11:18:27 -0500

    Update RoundPic.cs
    fixed format

--- RoundPic.cs
+++ RoundPic.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Drawing.Drawing2D;
 using System.Windows.Forms;
@@ -6,14 +6,15 @@ using System.Linq;
 using System.Text;
 using System.Drawing;
 
-
 namespace Calendar
 {
     class RoundPic : Button
     {
         public RoundPic()
         {
-            this.BackColor = Color.DarkGray; 
+            this.BackColor = Color.Transparent;
+            this.FlatStyle = FlatStyle.Flat;
+            this.FlatAppearance.BorderSize = 0;
         }
 
         protected override void OnPaint(PaintEventArgs e)
@@ -21,10 +22,18 @@ namespace Calendar
             GraphicsPath rgPath = new GraphicsPath();
             rgPath.AddEllipse(0, 0, ClientSize.Width, ClientSize.Height);
             this.Region = new Region(rgPath);
+
+            using (LinearGradientBrush brush = new LinearGradientBrush(this.ClientRectangle, Color.DarkGray, Color.LightGray, 45F))
+            {
+                e.Graphics.FillEllipse(brush, 0, 0, this.ClientSize.Width, this.ClientSize.Height);
+            }
+
+            using (Pen pen = new Pen(Color.Black, 2))
+            {
+                e.Graphics.DrawEllipse(pen, 0, 0, this.ClientSize.Width - 1, this.ClientSize.Height - 1);
+            }
+
             base.OnPaint(e);
         }
-
-
-
     }
 }
